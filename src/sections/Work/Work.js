62018e7a9c0d08e9985e8b23bec14b108d0cfc1d
Nyles-Ler <jlerat@rrc.ca>
2026-07-28 10:05:34 -0500

    Added Work section

--- src/sections/Work/Work.js
+++ src/sections/Work/Work.js
@@ -1,15 +1,51 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import { projects } from '../../data/projects';
+
+import ProjectCard from '../../components/ProjectCard/ProjectCard';
+
+const Section = styled.section`
+  background: #050816;
+  padding: 6rem 8%;
+`;
+
+const Heading = styled.h2`
+  color: white;
+  font-size: 3rem;
+  margin-bottom: 3rem;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+  gap: 3rem;
+`;
+
+const Intro = styled.p`
+  max-width: 1000px;
+  margin: -2rem 0 3rem;
+  color: #c4c9d4;
+  font-size: 1.05;
+  line-height: 1.7;
+`;
 
 const Work = () => {
   return (
-    <section id="work">
-      <h2>My Work</h2>
+    <Section id="work">
+      <Heading>Featured Projects</Heading>
 
-      <p>
-        A selection of projects completed throughout the Full Stack Web
-        Development program.
-      </p>
-    </section>
+      <Intro>
+        A Selection of fullstack, front-end, and development workflow projects
+        completed throughout my program.
+      </Intro>
+
+      <Grid>
+        {projects.map((project) => (
+          <ProjectCard key={project.title} project={project} />
+        ))}
+      </Grid>
+    </Section>
   );
 };
 
